2e95b99fca57fbef6eb3d68ee56b55dc77683517
majazaloznik <maja.zaloznik@gmail.com>
2016-07-29 19:22:43 +0100

    trying to automate wave/module reading. part 1

--- scripts/01.R
+++ scripts/01.R
@@ -5,8 +5,40 @@
 ## Preliminaries
 ###############################################################################
 require(foreign)
-test <- read.dta(file = "data/sharew1_rel5-0-0_ALL_datasets_stata/sharew1_rel5-0-0_cv_r.dta")
+require(dplyr)
+require(tidyr)
+## 
 
-test <- read.dta(file = "data/sharew5_rel5-0-0_ALL_datasets_stata/sharew5_rel5-0-0_xt.dta")
-head(test)
-table(test$xt012c)
+## first need to figure out the table of modules vs waves
+## so we have five wave folders
+data.dirs <- list.files("data", pattern = "*rel5-0-0_ALL_datasets_stata")
+waves<- as.numeric(substring(data.dirs,7,7))
+
+## extract module names from each wave folder
+FunWaveModExtract <- function(wave) {
+  x <- list.files(paste0("data/", data.dirs[wave]), pattern = ".dta")
+  paste(wave, substr(x, 18, regexpr(".dta", x)-1), sep="-")
+}
+
+## make nice df
+data.frame(combo = unlist(lapply(waves, function(x) FunWaveModExtract(x)))) %>%
+  separate(combo, into = c("wave", "module"), sep="-") %>%
+  table() %>% t() %>%
+  as.data.frame.matrix() -> lookup.module.wave
+
+## OK, so what am I doing. Lets get a balanced panel of the mental health and social 
+## Networks data, and see how far we get. 
+modules <- c("mh", "sn")
+waves <- c(1, 2, 3, 4, 5)
+
+lapply(waves, function(x) lapply(modules, function(y) {
+  if(lookup.module.wave[modules, waves][`y`,x]) print(y)
+  }
+))
+
+
+
+
+file.name <- paste0("data/sharew", wave, "_rel5-0-_ALL_datasets_stata/sharew", wave, "_rel5-0-0_", module, ".dta")
+
+
